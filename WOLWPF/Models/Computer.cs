d671ffc80dfe6a5422093a805701b8fe013b28b2
infernalred <39046593+infernalred@users.noreply.github.com>
2019-10-19 23:41:27 +0300

    add button

---
+++ WOLWPF/Models/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOLWPF.Models
+{
+    class Computer : INotifyPropertyChanged
+    {
+        public string IP { get { return this.IP; } set { this.IP = value; OnPropertyChanged("IP"); } }
+        public string Hostname { get { return this.Hostname; } set { this.Hostname = value; OnPropertyChanged("Hostname"); } }
+        public string MAC { get { return this.MAC; } set { this.MAC = value; OnPropertyChanged("MAC"); } }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ WOLWPF/ViewModels/ComputerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WOLWPF.Models;
+
+namespace WOLWPF.ViewModels
+{
+    class ComputerViewModel : INotifyPropertyChanged
+    {
+        private Computer selectedComputer;
+        public ObservableCollection<Computer> Computers { get; set; }
+        public Computer SelectedComputer 
+        {
+            get { return selectedComputer; }
+            set
+            {
+                selectedComputer = value;
+                OnPropertyChanged("SelectedComputer");
+            }
+        }
+        public ComputerViewModel()
+        {
+            Computers = new ObservableCollection<Computer>();
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
